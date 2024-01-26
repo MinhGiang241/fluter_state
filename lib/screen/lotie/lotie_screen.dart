7f9f7c315681e75a04f34051f66b30d6d8cd55a5
Minh Giang <minhgiang241@gmail.com>
2024-01-26 18:12:18 +0700

    lotie screen

--- lib/screen/lotie/lotie_screen.dart
+++ lib/screen/lotie/lotie_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LotieScreen extends StatelessWidget {
   const LotieScreen({super.key});
@@ -7,7 +8,38 @@ class LotieScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Lotie")),
-      body: Column(children: []),
+      body: GridView.count(
+          shrinkWrap: true,
+          crossAxisCount: 2,
+          mainAxisSpacing: 18,
+          crossAxisSpacing: 15,
+          childAspectRatio: 1,
+          children: [
+            Lottie.asset(
+              'assets/lottie/flower.json',
+              fit: BoxFit.contain,
+            ),
+            Lottie.asset(
+              'assets/lottie/car.json',
+              fit: BoxFit.contain,
+            ),
+            Lottie.asset(
+              'assets/lottie/bird.json',
+              fit: BoxFit.contain,
+            ),
+            Lottie.asset(
+              'assets/lottie/404.json',
+              fit: BoxFit.contain,
+            ),
+            Lottie.asset(
+              'assets/lottie/firework.json',
+              fit: BoxFit.contain,
+            ),
+            Lottie.asset(
+              'assets/lottie/house.json',
+              fit: BoxFit.contain,
+            ),
+          ]),
     );
   }
 }
